239f3b3215ef2d9dbf843d059f6ed44347bb4ccb
leo123nunes <ti.userleo@hotmail.com>
2020-12-17 18:02:01 -0300

    Adding digits to the display when click and AC functionallity

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,30 +3,60 @@ import './Calculator.css'
 import Button from '../components/Button.jsx'
 import Display from '../components/Display.jsx'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0
+}
+
 export default class Calculator extends Component{
+    state = {...initialState}
+
     constructor(props){
         super(props)
 
+        
         this.addDigit = this.addDigit.bind(props)
         this.setOperation = this.setOperation.bind(props)
         this.cleanMemory = this.cleanMemory.bind(props)
     }
 
-    addDigit(digit){
-        console.log(digit)
+    addDigit = (digit) => {
+        if(digit==='.' && this.state.displayValue.includes('.')){
+            return
+        }
+
+        var clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+
+        var currentValue = clearDisplay ? '' : this.state.displayValue
+
+        var displayValue = currentValue + digit
+
+        this.setState({displayValue, clearDisplay: false})
+
+        if(digit != '.'){
+            var index = this.state.current
+            var values = [...this.state.values]
+            values[index] = parseFloat(displayValue)
+            this.setState({values})
+        }
+
+
     }
 
     setOperation(operation){
         console.log(operation)
     }
 
-    cleanMemory(){
-        console.log('cleaning the memory')
+    cleanMemory = () => {
+        this.setState({...initialState})
     }
 
     render(){
         return <div className="calculator">
-            <Display value={100}></Display>
+            <Display value={this.state.displayValue}></Display>
             <Button label="AC" onclick={this.cleanMemory} triple></Button>
             <Button label="/" onclick={this.setOperation} operation></Button>
             <Button label="7" onclick={this.addDigit}></Button>
